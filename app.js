756ad55cb6c53b8a5a39234a09802ac7c43047f3
GrayOken <grayson.okenoas@gmail.com>
2023-08-16 13:32:32 -0700

    Did the assignment

---
+++ app.js
@@ -0,0 +1,16 @@
+let flavors = prompt(
+	"Please enter a comma separated list of flavors",
+	"vanilla,vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+).split(",");
+let flavorObj = {};
+
+flavors.forEach((flavor) => {
+	if (flavor in flavorObj) {
+		flavorObj[flavor] += 1;
+	} else {
+		flavorObj[flavor] = 1;
+	}
+});
+
+console.log(flavors);
+console.log(flavorObj);
